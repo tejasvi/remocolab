f837d835d4303572cc6f23d81c1bde05e49bb9d8
tejasvi <45873379+tejasvi@users.noreply.github.com>
2020-05-18 13:10:56 +0000

    Bug

--- remocolab.py
+++ remocolab.py
@@ -269,7 +269,7 @@ subprocess.run(
 (pathlib.Path.home() / ".xscreensaver").write_text("mode: off\\n")
 """)
   r = subprocess.run(
-                    ["su", "-c", "python3 " + str(vncrun_py), "colab"],
+                    ["su", "-c", "python3 " + str(vncrun_py), "root"],
                     check = True,
                     stdout = subprocess.PIPE,
                     universal_newlines = True)
